d12fd4aac8652802e15f89c673e71c8817f9388b
Jon <jbsunrise@reagan.com>
2022-08-16 13:00:40 -0400

    Fixed value derived from parent/child relationship Added popup menu to treeview

--- CLICommWindow_ElementTree.py
+++ CLICommWindow_ElementTree.py
@@ -15,21 +15,61 @@ class ElementTree(QtWidgets.QTreeWidget):
     self.setHeaderItem(QtWidgets.QTreeWidgetItem(columns_))
     aLayout.addLayout(l_, 0, 0)
     CLICommWindow_Yaml.LoadData_Yaml(self,'elements.yaml')
+    
+    self.popupItem = None
+    self.popup = QtWidgets.QMenu()
+    self.popup.addAction("Send", self.doSend)
+    # Connect the contextmenu
+    self.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+    self.customContextMenuRequested.connect(self.menuContextTree)
+
+
+  def menuContextTree(self, point):
+    # Infos about the node selected.
+    item_ = self.itemAt(point)
+    if item_:
+      self.popupItem = item_
+      self.popup.exec(self.mapToGlobal(point))
+
 
   def addTop(self, aValue, aColumns):
     item_ = QtWidgets.QTreeWidgetItem(aColumns)
     item_.setData(0, QtCore.Qt.UserRole, aValue)
-    self.itemDoubleClicked.connect(self.doDblClick)
+    #self.itemDoubleClicked.connect(self.doDblClick)
     self.addTopLevelItem(item_)
     return item_
     
-  def addChild( self, aParent, aValue, aColumns):
+  def addGroup( self, aParent, aColumns):
     item_ = QtWidgets.QTreeWidgetItem( aParent, aColumns )
-    item_.setData(0, QtCore.Qt.UserRole, aValue)
     return item_
 
-  def doDblClick(self, aItem):
+  def addChild( self, aParent, aValue, aColumns):
+    item_ = QtWidgets.QTreeWidgetItem( aParent, aColumns )
+    self.itemDoubleClicked.connect(self.doDblClick)
+    item_.setData(0, QtCore.Qt.UserRole, aValue)
+
+  def _getData(self, aItem):
+    ret_ = None
     value_ = aItem.data(0, QtCore.Qt.UserRole )
-    self.owner.entrybar.setValue( value_ )
+    if value_:
+      parent_ = aItem.parent()
+      parentValue_ = parent_.data(0, QtCore.Qt.UserRole )
+      if parentValue_ == None:
+        parent_ = parent_.parent()
+        if parent_:
+          parentValue_ = parent_.data(0, QtCore.Qt.UserRole )
+          
+      if parentValue_ != None:
+        ret_ = "%s %s" % (parentValue_, value_ )
+    return ret_
+
+  def doDblClick(self, aItem):
+    ret_ = self._getData(aItem)
+    if ret_:
+      self.owner.entrybar.setValue( ret_ )
     
+  def doSend(self):
+    ret_ = self._getData(self.popupItem)
+    if ret_:
+      self.owner.entrybar.setValue( ret_ )
     
--- CLICommWindow_Yaml.py
+++ CLICommWindow_Yaml.py
@@ -10,29 +10,25 @@ class LoadData_Yaml():
       for category_ in ydata_['Categories']:
         #print(category_['name'])
         categoryValue_ = category_['value']
-#        parent_ = tv.insert('', "end", text="", values=(category_['name'], "", "", category_['help']), open=True)
-        parent_ = aOwner.addTop( "", (category_['name'], "", "", category_['help']) )
+        parent_ = aOwner.addTop( categoryValue_, (category_['name'], "", "", category_['help']) )
         for element_ in category_['Elements']:
           #print(element_)
           #print( "%s %s" % (element_['name'], element_['value']) )
           help_ = ''
           if 'Help' in element_:
             help_ = element_['Help']
-#          tv.insert(parent_, "end", text="%s %s" % (categoryValue_, element_['value']), values=("", "", element_['name'], help_), open=True)
-          aOwner.addChild( parent_, "%s %s" % (categoryValue_, element_['value']), ("", "", element_['name'], help_) )
+          aOwner.addChild( parent_, element_['value'], ("", "", element_['name'], help_) )
         if 'Group' in category_:
           for group_ in category_['Group']:
             help_ = ''
             if 'Help' in group_:
               help_ = group_['Help']
-            #parent1_ = tv.insert(parent_, "end", text="", values=("", group_['name'], "", help_), open=True)
-            parent1_ = aOwner.addChild( parent_, "", ("", group_['name'], "", help_) )
+            parent1_ = aOwner.addGroup( parent_, ("", group_['name'], "", help_) )
             for element_ in group_['Elements']:
               #print(element_)
             #print( "%s %s" % (element_['name'], element_['value']) )
               help_ = ''
               if 'Help' in element_:
                 help_ = element_['Help']
-              #tv.insert(parent1_, "end", text="%s %s" % (categoryValue_, element_['value']), values=("", "", element_['name'], help_), open=True)
-              aOwner.addChild( parent1_, "%s %s" % (categoryValue_, element_['value']), ("", "", element_['name'], help_) )
+              aOwner.addChild( parent1_, element_['value'], ("", "", element_['name'], help_) )
 
