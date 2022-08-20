04fb50aef39cc23d3fdd236750ebf6b12d9a61ee
Jon <jbsunrise@reagan.com>
2022-08-19 21:36:40 -0400

    Selected text background set to yellow

--- CLICommWindow_ReceivedText.py
+++ CLICommWindow_ReceivedText.py
@@ -14,6 +14,16 @@ class ReceivedText(QtWidgets.QTextEdit):
     curs_.movePosition(QtGui.QTextCursor.End)
     curs_.insertText(aString)
 
-  def doFind(self,aString):
+  def doFind(self,aString, aNext):
+    if False == aNext:
+      cursor_ = self.textCursor()
+      cursor_.movePosition( QtGui.QTextCursor.Start )
+      self.setTextCursor(cursor_)
     self.find(aString)
-    print( self.textCursor().selectedText() )
+    cursor_ = self.textCursor()
+    print( cursor_.selectedText() )
+    fmt_ = cursor_.charFormat()
+    fmt_.setBackground(QtCore.Qt.yellow)
+    fmt_.setBackground(QtCore.Qt.yellow) 
+    cursor_.setCharFormat(fmt_)
+    
